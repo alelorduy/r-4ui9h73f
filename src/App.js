ee13a801cef1458542db858a66ff3d42c3c535f1
alelorduy <alejandra_lorduy@hotmail.com>
2021-05-13 20:22:10 -0400

    Create function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
-      </div>
-    );
+const App = () => {
+  const [value, setValue] = useState("");
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setValue(value);
   }
-}
+
+  return (
+    <div>
+      <h1>Un repetidor</h1>
+      <input
+        type="text"
+        placeholder="Empieza a escribir algo"
+        value={value}
+        onChange={handleChange}
+      />
+      <p className="repeater">{value}</p>
+    </div>
+  );
+};
 
 export default App;
